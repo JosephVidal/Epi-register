36eb3a126213cf721512e82ec09ae1158e41c196
Joseph <joseph.vidal@epitech.eu>
2022-10-05 17:46:58 +0200

    [MOD] Handling "-t" option + new log system

--- register.py
+++ register.py
@@ -3,39 +3,55 @@
 
 import sys
 import requests
+import random
 
+from rich.theme import Theme
+from rich.console import Console
 from time import sleep
+
+theme = Theme({"success": "green", "error": "bold red", "neutral": "white"})
+console = Console(theme=theme)
+
 
 def register(session, url, cookies, payload, module):
     for elem in cookies:
-        obj             = elem.split('=')
+        obj = elem.split('=')
         payload[obj[0]] = obj[1]
     rep = session.post(url, cookies=payload)
     if (rep.status_code == 200):
-        print("Registrationn succeed to " + module)
+        console.log("Registration succeed to " + module, theme="success")
         return True
-    print("Failed to register to " + module + " (code " + str(rep.status_code) + ")")
+    console.log("Failed to register to " + module + " (code " + str(rep.status_code) + ")", style="error")
     return False
 
+
 def main(args):
-    cookies = args[args.index("-c") + 1].replace(' ', '').split(";") # ex : "foo=bar; name=Jhon; lastname=Doe"
+    cookies = args[args.index("-c") + 1].replace(' ', '').split(";")  # ex : "foo=bar; name=Jhon; lastname=Doe"
     modules = args[1:args.index("-c")]
     payload = {}
     session = requests.Session()
+    time = float(args[args.index("-t") + 1]) if "-t" in args else 120
+    step = 0
 
-    while 1 :
-        for module in modules:
-            url = "https://intra.epitech.eu/module/2022/" + module + "/PAR-9-2/register?format=json"
-            if register(session, url, cookies, payload, module) == False:
-                continue
-            modules.remove(module)
-        if (len(modules) == 0):
-            break
-        sleep(1)
+    console.log(f"Timer: {time} seconds", style="neutral")
+    with console.status("[bold green]Progressing...") as status:
+        while 1:
+            step = step + 1
+            console.log("try " + str(step), style="success")
+            for module in modules:
+                url = "https://intra.epitech.eu/module/2022/" + module + "/PAR-9-2/register?format=json"
+
+                if register(session, url, cookies, payload, module):
+                    modules.remove(module)
+            if (len(modules) == 0):
+                break
+            sleep(time)
+    console.log('Done!', style="success")
+
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("USAGE:\n\t./register <module_id> [-c <cookies>]\n\nEXAMPLE\n\t./register M-BDX-001 M-PRO-045 M-TRV-014 M-PRO-002 -c \"foo=bar; name=Jhon; lastname=Doe\"")
+    if len(sys.argv) < 3 or not "-c" in sys.argv:
+        print("USAGE:\n\t./register <module_id> -c <cookies> [-t <time>]\n\nEXAMPLE\n\t./register M-BDX-001 M-PRO-045 M-TRV-014 M-PRO-002 -c \"foo=bar; name=Jhon; lastname=Doe\"")
         sys.exit(84)
     else:
         main(sys.argv)
